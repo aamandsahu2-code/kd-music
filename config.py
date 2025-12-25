053d2266b8830221d2416c4069741f739c586360
aamandsahu2-code <aamandsahu2@gmail.com>
2025-12-25 19:05:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+VC_SESSION = os.getenv("VC_SESSION")
